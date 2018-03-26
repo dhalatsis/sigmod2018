0000e166efdb27b9bb9fe96444763ed1431eb36b
Theofilos <theofiasp@gmail.com>
2018-03-26 16:13:44 +0300

    m

--- quick-start-package/include/tbb_parallel_types.hpp
+++ quick-start-package/include/tbb_parallel_types.hpp
@@ -1,14 +1,16 @@
 
 #include "tbb/tbb.h"
 #include "tbb/parallel_reduce.h"
+#include "tbb/parallel_for.h"
 #include "tbb/blocked_range.h"
+#include "tuple_buffer.h"
 
 
 using namespace tbb;
 
 /* Struct for praralle check sum */
-struct CheckSumT
-{
+struct CheckSumT {
+
 public:
     uint64_t my_sum;
 
@@ -24,6 +26,7 @@ public:
     /* How to join the thiefs */
     void join(const CheckSumT & y) {my_sum += y.my_sum;}
 
+    /* The function call overloaded operator */
     void operator()(const tbb::blocked_range<size_t>& range) {
         uint64_t sum = my_sum;
         for (size_t i = range.begin(); i < range.end(); ++i)
@@ -38,3 +41,43 @@ private:
     unsigned   rels_num;
     int  tbi;
 };
+
+/* Struct for parallelizing create table t */
+#ifdef g
+struct TableConstructionT {
+
+public:
+    uint64_t my_sum;
+
+    /* Initial constructor */
+    TableConstructionT(uint64_t * dataPtr, unsigned * row_ids, unsigned rnum, int idx)
+    :col{dataPtr}, rids{row_ids}, rels_num{rnum}, tbi{idx}, my_sum(0)
+    {}
+
+    /* Slpitting constructor */
+    TableConstructionT(TableConstructionT & x, split)
+    :col{x.col}, rids{x.rids}, rels_num{x.rels_num}, tbi{x.tbi}, my_sum(0) {}
+
+    /* How to join the thiefs */
+    void join(const TableConstructionT & y) {my_sum += y.my_sum;}
+
+    /* The function call overloaded operator */
+    void operator()(const tbb::blocked_range<size_t>& range) {
+        uint64_t sum = my_sum;
+        for (size_t i = range.begin(); i < range.end(); ++i)
+            sum += col[rids[i*rels_num + tbi]];
+
+        my_sum = sum;
+    }
+
+private:
+    uint64_t * col;
+    tuplebuffer_t * tb
+    unsigned * new_rids;
+    unsigned * rids_r;
+    unsigned * rids_s;
+    unsigned   rels_num;
+    std::vector<unsigned> * help_v_r;
+    std::vector<unsigned> * help_v_s;
+};
+#endif
