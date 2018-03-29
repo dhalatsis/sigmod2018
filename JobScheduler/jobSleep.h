1cd1037f54b97758c84bf8edf209cd72ad6d5732
Vaggelis Nikolopoulos <vgnikolop@gmail.com>
2018-03-29 23:26:52 +0300

    adding Job scheduler

---
+++ JobScheduler/jobSleep.h
@@ -0,0 +1,34 @@
+#ifndef JOBSLEEP_H_
+#define JOBSLEEP_H_
+
+#include "job.h"
+#include <string>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+
+namespace mple {
+
+class JobSleep: public mple::Job {
+public:
+	int time_s;
+	int jid;
+
+	int Run(){
+		std::cout << "Job " << jid << " is going to sleep for " <<  time_s << " secs..." << std::endl;
+		std::this_thread::sleep_for(std::chrono::seconds(time_s));
+		std::cout << "Job " << jid << " done!" << std::endl;
+	}
+
+	JobSleep(int job_id, int time) {
+		time_s = time;
+		jid = job_id;
+	}
+
+	virtual ~JobSleep(){}
+	
+};
+
+} /* namespace mple */
+#endif /* JOBPRINT_H_ */
