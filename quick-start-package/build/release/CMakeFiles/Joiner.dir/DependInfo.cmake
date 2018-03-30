3e63535d08924efc004e328f8d8afe1e1870c089
Orest Gkini <orestijuve@gmail.com>
2018-03-30 14:37:32 +0300

    G

---
+++ quick-start-package/build/release/CMakeFiles/Joiner.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/Filter.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/Filter.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/JobScheduler/jobBarrier.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/JobScheduler/jobBarrier.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/JobScheduler/job_scheduler.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/JobScheduler/job_scheduler.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/Joiner.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/Joiner.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/Parser.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/Parser.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/QueryPlan.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/QueryPlan.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/Relation.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/Relation.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/Utils.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/Utils.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/cpu_mapping.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/cpu_mapping.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/generator.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/generator.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/parallel_radix_join.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/parallel_radix_join.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/sys.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/sys.cpp.o"
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/treegen.cpp" "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/Joiner.dir/treegen.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "/home/orestis/tbb/tbb2018_20171205oss/include"
+  "../../JobScheduler"
+  "../../JobScheduler/sparsehash-2.0.2/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/orestis/Desktop/sigmod2018/quick-start-package/build/release/CMakeFiles/database.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ quick-start-package/build/release/CMakeFiles/Joiner.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Joiner.dir/Joiner.cpp.o"
+  "CMakeFiles/Joiner.dir/QueryPlan.cpp.o"
+  "CMakeFiles/Joiner.dir/treegen.cpp.o"
+  "CMakeFiles/Joiner.dir/sys.cpp.o"
+  "CMakeFiles/Joiner.dir/parallel_radix_join.cpp.o"
+  "CMakeFiles/Joiner.dir/cpu_mapping.cpp.o"
+  "CMakeFiles/Joiner.dir/generator.cpp.o"
+  "CMakeFiles/Joiner.dir/Filter.cpp.o"
+  "CMakeFiles/Joiner.dir/Relation.cpp.o"
+  "CMakeFiles/Joiner.dir/Parser.cpp.o"
+  "CMakeFiles/Joiner.dir/Utils.cpp.o"
+  "CMakeFiles/Joiner.dir/JobScheduler/job_scheduler.cpp.o"
+  "CMakeFiles/Joiner.dir/JobScheduler/jobBarrier.cpp.o"
+  "Joiner.pdb"
+  "Joiner"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Joiner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
